077267bfb37812588e6a92b377037c172db09f81
codeZy1 <88177813+codeZy1@users.noreply.github.com>
2022-01-02 15:31:05 +0800

    Update notsupported.lua

--- notsupported.lua
+++ notsupported.lua
@@ -1,1 +1,6 @@
-print("game not supported")
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/exploiting/main/EngoUILIB_V2.lua"))()
+local main = library:CreateMain("", "Executor: "..identifyexecutor().."!")
+
+main:CreateNotification("Alert!", "Game not supported!" , function()
+end) 
