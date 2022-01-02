0f0c5d332d98f3a8a9c249487540e7ef10541c16
codeZy1 <88177813+codeZy1@users.noreply.github.com>
2022-01-02 15:22:01 +0800

    Update notsupported.lua

--- notsupported.lua
+++ notsupported.lua
@@ -1,6 +1,7 @@
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/joeengo/exploiting/main/EngoUILIB_V2.lua"))()
 local main = library:CreateMain("Prison life", "Executor: "..identifyexecutor().."!")
+local Tab1 = main:CreateTab("Not supported")
 
 main:CreateNotification("Alert!", "This game is not supported" , function()
 end) 
