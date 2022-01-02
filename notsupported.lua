002a0ebc40663f7f7b72d17222ec66db2b723b11
codeZy1 <88177813+codeZy1@users.noreply.github.com>
2022-01-02 15:28:12 +0800

    Update notsupported.lua

--- notsupported.lua
+++ notsupported.lua
@@ -1,6 +1,1 @@
-wait(1)
-game.StarterGui:SetCore("SendNotification", {
-Title = "Game not supported"; -- the title (ofc)
-Text = "Game not supported"; -- what the text says (ofc)
-Duration = 5; -- how long the notification should in secounds
-})
+print("game not supported")
